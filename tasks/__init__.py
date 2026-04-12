370956e29a71b20e490a6b01499d111190a53dae
Thirth Patel <thirthpatel2@gmail.com>
2026-04-12 14:02:18 +0530

    fix: recreate __init__.py as valid utf8 empty file

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,5 +1,1 @@
-from tasks.task_easy import run_task as easy
-from tasks.task_medium import run_task as medium
-from tasks.task_hard import run_task as hard
-
-__all__ = ["easy", "medium", "hard"]
+﻿
